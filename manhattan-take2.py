d2f408ff8b3a19bdd67bb61d0c6c6478496c296a
Nabira Rashid <163450706+nabirarashid@users.noreply.github.com>
2025-01-01 00:15:03 -0500

    Update manhattan-final.py

--- manhattan-take2.py
+++ manhattan-take2.py
@@ -7,17 +7,17 @@ from sklearn.model_selection import train_test_split
 df = pd.read_csv("https://raw.githubusercontent.com/Codecademy/datasets/master/streeteasy/manhattan.csv")
 
 #prints first 5 rows of the dataframe
-print(df.head())
+# print(df.head())
 
 #split into training (to fit data) & testing (to test model!)
 #need 2 brackets when multiple columns from dataframe
 
-x = df["size_sqft"]
+x = df[["size_sqft"]]
 y = df["rent"]
 
 x_train, x_test, y_train, y_test = train_test_split(x,y,train_size=0.8,test_size=0.2,random_state=6)
 
-print(x_train, x_test, y_train, y_test)
+# print(x_train, x_test, y_train, y_test)
 
 # this is the model!
 mlr = LinearRegression()
@@ -28,10 +28,10 @@ results = pd.DataFrame(x_test)
 results['y_test'] = y_test.values
 results['y_predict'] = y_predict
 
-sns.scatterplot(x=x_test, y=y_test, label="actual data", color="red")
-sns.lineplot(x=x_test, y=y_predict, label="predicted data", color="blue")
+# changing x values to 1D array
+sns.scatterplot(x=x_test.squeeze(), y=y_test, label="actual data", color="red")
+sns.lineplot(x=x_test.squeeze(), y=y_predict, label="predicted data", color="blue")
 plt.xlabel("Size sqft")
 plt.xlabel("Rent")
 plt.title("Actual Rent vs Predicted Rent")
 plt.show()
-
